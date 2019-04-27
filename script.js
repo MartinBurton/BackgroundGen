3d32eadd84558366151c881bfa05b0c57efd3187
Dewpoint-Martin <martin_burton@hotmail.com>
2019-04-27 18:21:58 +0100

    Added Lodash to the project

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var _ = require('lodash');
+
+console.log(_);
+
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
@@ -17,5 +21,3 @@ function setGradient() {
 
     css.textContent = body.style.background + ";";
 }
-
-body.addEventListener("onpageshow", setGradient);
